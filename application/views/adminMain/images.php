39d11de33755406fdeda8b7e3e8542942cdea751
zh <dimajkezh@gmail.com>
2018-06-29 04:15:19 +0300

    zh_photos

--- application/views/adminMain/images.php
+++ application/views/adminMain/images.php
@@ -1,1 +1,81 @@
-<?php //debug($IMAGES); ?>
+		<div class="content_wrapper">
+			<div class="main_content_head">
+				<p class="main_content_head_title">Список фотографий</p>
+				<div class="main_content_head_settings">
+					<form>
+						<input type="text" placeholder="Значение тэга">
+						<input type="text" placeholder="Название тэга">
+						<input type="reset" class="btn red"	>
+					</form>
+					<button onclick="modalOpen()" class="btn green">Добавить</button>
+				</div>
+			</div>
+			<div class="main_content_info">
+				<div class="photo_list">
+					<div class="photo_list_item">
+						<img src="/assets/img/de.png" alt="">
+						<button class="btn red">
+							<span>X</span>
+						</button>
+					</div>
+					<div class="photo_list_item">
+						<img src="/assets/img/china.png" alt="">
+						<button class="btn red">
+							<span>X</span>
+						</button>
+					</div>
+					<div class="photo_list_item">
+						<img src="/assets/img/korea.png" alt="">
+						<button class="btn red">
+							<span>X</span>
+						</button>
+					</div>
+					<div class="photo_list_item">
+						<img src="/assets/img/pizza.png" alt="">
+						<button class="btn red">
+							<span>X</span>
+						</button>
+					</div>
+					<div class="photo_list_item">
+						<img src="/assets/img/russia.png" alt="">
+						<button class="btn red">
+							<span>X</span>
+						</button>
+					</div>
+					<div class="photo_list_item">
+						<img src="/assets/img/sweden.png" alt="">
+						<button class="btn red">
+							<span>X</span>
+						</button>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="modal_wnd">
+			<div class="modal_wnd_wrapper" id="wrap" onclick="cancelUpload()">
+			</div>
+			<div class="modal_wnd_inner" id="window">
+				<div class="modal_wnd_head">
+					<button onclick="cancelUpload()" class="btn red">Отмена</button>
+					<button onclick="goUpload()"" class="btn green">Загрузить</button>
+				</div>
+				<div class="modal_wnd_content">
+					<div class="modal_drug_and_drop">
+						<div id="drop-area">
+	  						<form class="my-form">
+	   					 		<p>Загрузите несколько файлов сразу, просто перетащив изображения в модальное окно</p>
+	   					 		<input type="file" id="fileElem" multiple accept="image/*" onchange="handleFiles(this.files)">
+	   					 		<label class="button" for="fileElem">Выбирите несколько файлов</label>
+	  						</form>
+	  						<progress id="progress-bar" max=100 value=0></progress>
+	  						<div class="gallery_wrapper">
+		  						<div id="gallery">
+	  							</div>
+	  						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<script src="/assets/js/drop.js"></script>
+		</div>
---
+++ assets/js/drop.js
@@ -0,0 +1,155 @@
+	/* массив файлов */
+	var FILES = [];
+	// ************************ Drag and drop ***************** //
+	let dropArea = document.getElementById("drop-area")
+
+	// Prevent default drag behaviors
+	;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+	  dropArea.addEventListener(eventName, preventDefaults, false)   
+	  document.body.addEventListener(eventName, preventDefaults, false)
+	})
+
+	// Highlight drop area when item is dragged over it
+	;['dragenter', 'dragover'].forEach(eventName => {
+	  dropArea.addEventListener(eventName, highlight, false)
+	})
+
+	;['dragleave', 'drop'].forEach(eventName => {
+	  dropArea.addEventListener(eventName, unhighlight, false)
+	})
+
+	// Handle dropped files
+	dropArea.addEventListener('drop', handleDrop, false)
+
+	function preventDefaults (e) {
+	  e.preventDefault()
+	  e.stopPropagation()
+	}
+
+	function highlight(e) {
+	  dropArea.classList.add('highlight')
+	}
+
+	function unhighlight(e) {
+	  dropArea.classList.remove('active')
+	}
+
+	function handleDrop(e) {
+	  var dt = e.dataTransfer
+	  var files = dt.files
+
+	  handleFiles(files)
+	}
+
+	/* переменные для прогресс-бара */
+	let uploadProgress = []
+	let progressBar = document.getElementById('progress-bar')
+
+	/* инициализация прогресс-бара */
+	function initializeProgress(numFiles) {
+	  progressBar.value = 0
+	  uploadProgress = []
+
+	  for(let i = numFiles; i > 0; i--) {
+	    uploadProgress.push(0)
+	  }
+	}
+
+	/* обновление прогресс-бара */
+	function updateProgress(fileNumber, percent) {
+	  uploadProgress[fileNumber] = percent
+	  let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
+	  //console.debug('update', fileNumber, percent, total)
+	  progressBar.value = total
+	}
+
+	/* Загрузка файлов в массив */
+	function handleFiles(files) {
+	  	files = [...files];
+	  	files.forEach(function(file){
+	  		if(FILES.length < 1){
+				file.id = 0;
+	  		}else{
+	  			let max = 0;
+	  			FILES.forEach(function(val){
+	  				if(val.id > max){
+	  					max = val.id;
+	  				}
+	  			});
+	  			file.id = max+1;
+	  		}
+	  		FILES.push(file);
+
+	  	});
+	  	files.forEach(previewFile);
+	}
+
+	/* Добавить файл в превью */
+	function previewFile(file) {
+	  let reader = new FileReader()
+	  reader.readAsDataURL(file)
+	  reader.onloadend = function() {
+	    let img = document.createElement('img')
+	    img.src = reader.result
+	    let box = document.createElement('div');
+	    box.classList.add('img_box');
+	    box.appendChild(img);
+	    let btn = document.createElement('button');
+	    btn.classList.add('btn');
+	    btn.classList.add('red');
+	    btn.innerHTML = '<p>X</p>';
+	    btn.onclick=function(){deleteElement(this, file.name, file.id)};
+	    box.appendChild(btn);
+	    document.getElementById('gallery').appendChild(box)
+	  }
+	}
+	/* загрузка файла на сервер */
+	function uploadFile(file, i) {
+	  var url = 'loader.php'
+	  var xhr = new XMLHttpRequest()
+	  var formData = new FormData()
+	  xhr.open('POST', url, true)
+	  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+	  xhr.upload.addEventListener("progress", function(e) {
+	    updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
+	  })
+	  xhr.addEventListener('readystatechange', function(e) {
+	    if (xhr.readyState == 4 && xhr.status == 200) {
+	      	updateProgress(i, 100);
+	    	console.log('good '+i);
+	    }
+	    else if (xhr.readyState == 4 && xhr.status != 200) {
+	     	console.log('error '+i);
+	    }
+	  })
+	  formData.append('ID', file.id);
+	  formData.append('file', file);
+	  xhr.send(formData);
+	}
+
+	/* кнопка загрузить */
+	function goUpload(){
+		//console.log(FILES);
+		if(FILES != undefined){
+			initializeProgress(FILES.length);
+			FILES.forEach(uploadFile);
+		}	
+	}
+
+	/* кнопка удаления элемента из массива */
+	function deleteElement(THIS, NAME, ID){
+		FILES.forEach(function(val, key){
+			if((val.name === NAME) && (val.id == ID)){
+				FILES.splice(key, 1);
+				return;
+			}
+		});
+		THIS.parentNode.remove();
+	}
+
+	/* кнопка отмены */
+	function cancelUpload(){
+		FILES = [];
+		document.getElementById('gallery').innerHTML = '';
+		modalClose();
+	}
